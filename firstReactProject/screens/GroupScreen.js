a5998783db0c07298800f6bfa71501279513c840
Erlend Skansen <skaerl17@EGMS.no>
2018-12-06 09:56:39 +0100

    Added firstReactProject
    my own testing folder

---
+++ firstReactProject/screens/GroupScreen.js
@@ -0,0 +1,46 @@
+/*
+import React from 'react';
+import {
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { ExpoConfigView } from '@expo/samples';
+
+export default class SettingsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'app.json',
+  };
+
+  render() {
+    return (
+      <View style={}>
+        <ScrollView style={}>
+          <View style={styles.topBar}>
+
+            //add views(icons/text) to topBar
+
+          </View>
+
+          <View style={styles.groupsContainer}>
+            <View style={{height: 100}}>
+              <FlatList style={styles.chatListItem}
+                        data={chatFlatListData} //Create this class
+                        renderItem={({item, index}) => {
+                          return (
+                            //Export from chatFlatListData
+                            <ChatFlatListItem item={item} index={index} parentFlatList={this}>
+                            </ChatFlatListItem>
+                          )
+                        }}>
+              </FlatList>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+*/
