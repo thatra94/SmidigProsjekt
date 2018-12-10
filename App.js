5ca6cfe7f0bbab9077512986d2c314da2bad7802
Erlend Skansen <skaerl17@EGMS.no>
2018-12-10 12:41:44 +0100

    Updated project

--- App.js
+++ App.js
@@ -1,64 +1,105 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-import firebase from 'react-native-firebase';
+import * as firebase from 'firebase';
+
+import {Container, Content, Header, Form, Input, Item, Button, Label} from 'native-base';
+
+//Initialize firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA8WS2uhrgoz0ldb-Uke0Uz0fv_cVfQehU",
+    authDomain: "smidigprosjekt-e3cdc.firebaseapp.com",
+    databaseURL: "https://smidigprosjekt-e3cdc.firebaseio.com",
+    projectId: "smidigprosjekt-e3cdc",
+    storageBucket: "smidigprosjekt-e3cdc.appspot.com",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
+
+  constructor(props){
+    super(props)
+
+    this.state = ({
+      email: '',
+      password: ''
+    })
   }
 
-  async componentDidMount() {
-    // TODO: You: Do firebase things
-    // const { user } = await firebase.auth().signInAnonymously();
-    // console.warn('User -> ', user.toJSON());
+  signUpUser = (email, password) => {
 
-    // await firebase.analytics().logEvent('foo', { bar: '123'});
+    try{
+
+      if (this.state.password.length < 6) {
+        alert("Please enter a password with length of atleast 6 characters")
+        return;
+      }
+
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+    }
+    catch(error){
+      console.log(error.toString())
+    }
+  }
+
+  loginUser = (email, password) => {
+
+    try{
+
+      firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
+        console.log(user)
+      })
+
+    }
+    catch(error) {
+      console.log(error.toString())
+    }
   }
 
   render() {
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
-          <Text style={styles.welcome}>
-            Welcome to {'\n'} React Native Firebase
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-          {Platform.OS === 'ios' ? (
-            <Text style={styles.instructions}>
-              Press Cmd+R to reload,{'\n'}
-              Cmd+D or shake for dev menu
-            </Text>
-          ) : (
-            <Text style={styles.instructions}>
-              Double tap R on your keyboard to reload,{'\n'}
-              Cmd+M or shake for dev menu
-            </Text>
-          )}
-          <View style={styles.modules}>
-            <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
-            {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
-            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
-            {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
-            {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
-            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
-            {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
-            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
-            {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
-            {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
-            {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
-            {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
-            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
-            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
-            {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
-            {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
-          </View>
-        </View>
-      </ScrollView>
+      <Container style={styles.container}>
+        <Form>
+
+          <Item floatingLabel>
+            <Label> Email </Label>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={(email) => this.setState({email})}
+            />
+          </Item>
+
+          <Item floatingLabel>
+            <Label> Password </Label>
+            <Input
+              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={(password) => this.setState({password})}
+            />
+          </Item>
+
+          <Button style={{marginTop: 40}}
+            full
+            rounded
+            success
+            onPress={()=> this.loginUser(this.state.email, this.state.password)}
+          >
+            <Text> Login </Text>
+          </Button>
+
+          <Button style={{marginTop: 20}}
+            full
+            rounded
+            primary
+            onPress={()=> this.signUpUser(this.state.email, this.state.password)}
+          >
+            <Text> Sign Up </Text>
+          </Button>
+        </Form>
+      </Container>
     );
   }
 }
@@ -66,37 +107,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 10
   },
-  logo: {
-    height: 120,
-    marginBottom: 16,
-    marginTop: 64,
-    padding: 10,
-    width: 135,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  modules: {
-    margin: 20,
-  },
-  modulesHeader: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  module: {
-    fontSize: 14,
-    marginTop: 4,
-    textAlign: 'center',
-  }
 });
