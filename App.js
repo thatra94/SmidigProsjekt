197808fb5766b1a72427003147870dadb3b4ad2a
Erlend Skansen <skaerl17@EGMS.no>
2018-12-10 09:30:11 +0100

    initialized firebase

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import styles from '../StyleSheets/styles.js'
+
 export default class App extends React.Component {
   render() {
     return (
@@ -11,11 +13,16 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+<script src="https://www.gstatic.com/firebasejs/5.7.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA8WS2uhrgoz0ldb-Uke0Uz0fv_cVfQehU",
+    authDomain: "smidigprosjekt-e3cdc.firebaseapp.com",
+    databaseURL: "https://smidigprosjekt-e3cdc.firebaseio.com",
+    projectId: "smidigprosjekt-e3cdc",
+    storageBucket: "smidigprosjekt-e3cdc.appspot.com",
+    messagingSenderId: "995575536754"
+  };
+  firebase.initializeApp(config);
+</script>
