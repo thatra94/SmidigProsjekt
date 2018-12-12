269104e0272d9a28d96bf6805b25246cfb912d72
Erlend Skansen <skaerl17@EGMS.no>
2018-12-12 12:15:49 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import AppNavigator from '../navigation/AppNavigator';
+//import AppNavigator from '../navigation/AppNavigator';
 
 import * as firebase from 'firebase';
 
