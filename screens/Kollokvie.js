099ac854431682e78eba5a7c1ecb7450be73941c
erlandoss <32767083+erlandoss@users.noreply.github.com>
2018-12-13 22:08:37 +0100

    StackNavigator that can take data input too change route

--- screens/Kollokvie.js
+++ screens/Kollokvie.js
@@ -40,7 +40,14 @@ export default class Fag extends React.Component {
             id: 8,
             name: 'Animasjon',
          },
-
+         {
+           id: 9,
+           name: 'Test'
+         },
+         {
+           id: 10,
+           name: 'Gruppe'
+         },
       ]
    }
    alertItemName = (item) => {
@@ -55,8 +62,7 @@ export default class Fag extends React.Component {
                   <TouchableOpacity
                      key = {item.id}
                      style = {styles.container}
-                     onPress={() => this.props.navigation.navigate('Gruppe')}
-                     //onPress = {() => this.alertItemName(item)}
+                     onPress={() => this.props.navigation.navigate(item.name)}
                      >
                      <Text style = {styles.text}>
                         {item.name}
@@ -72,9 +78,12 @@ export default class Fag extends React.Component {
 const styles = StyleSheet.create ({
    container: {
       padding: 30,
-      marginTop: 10,
+      marginTop: 0,
       backgroundColor: '#ffffff',
       alignItems: 'center',
+      borderWidth: 0.3,
+      borderColor: 'black',
+
    },
    text: {
       color: '#4f603c',
