8453d6a6e721bdca054f2764a5baf7ab5f094c02
fredriklugg <frelugg@gmail.com>
2018-12-14 10:17:19 +0100

    matching shit som ikke funker
    det funker ikke som det skal for faen

--- components/FireBase.js
+++ components/FireBase.js
@@ -34,3 +34,46 @@ export default class FireBase extends React.Component {
     });
   });*/
 }
+
+
+var testUserId = 123;
+//createNewGroup(testUserId);
+function joinGroup(userId){
+  console.log("#############Sjekker grupper###########");
+  if (!checkGroup(userId)) {
+    createNewGroup(testUserId);
+  }
+}
+
+function checkGroup(userId){
+
+  var query = firebase.database().ref("database/");
+  query.once("value", function(snapshot) {
+      snapshot.forEach(function(snapshot) {
+          console.log(snapshot.key);
+          var userCount = snapshot.val().userCount;
+          var groupKey = snapshot.key;
+          if(userCount < 3){
+            addToGroup(groupKey, userId);
+            return true;
+          }
+          return false;
+      });
+  });
+}
+
+function addToGroup(groupKey, userId){
+    firebase.database().ref('database/'+groupKey).update({
+      Token: userId,
+      userCount: userCount+1,
+    });
+    console.log("Lagt til i gruppe!");
+}
+function createNewGroup(userId){
+  firebase.database().ref('database/').push({
+    userCount: 0,
+    userToken: true,
+    Token: userId
+  });
+  console.log("Lagd ny gruppe");
+}
