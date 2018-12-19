b9d62f69117e43b72ed9e1c126f3be5669b8ff1e
thatra94 <38081855+thatra94@users.noreply.github.com>
2018-12-19 10:24:08 +0100

    fixed signup

--- screens/login/SignUp.js
+++ screens/login/SignUp.js
@@ -3,16 +3,72 @@ import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import firebase from 'firebase';
 
 export default class SignUp extends React.Component {
-  state = { email: '', password: '', errorMessage: null }
+  state = { email: '', password: '', firstName: '', lastName:'', studySubject: '', errorMessage: null }
+
+/*
+componentDidMount() {
+  this.authListener();
+}
+
+authListener() {
+  const { email, firstName} = this.state
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log("We are authenticated now!");
+      useruid = user.uid,
+      console.log(useruid),
+      firebase.database().ref('users/' + useruid).set({
+        email: email.toLowerCase(),
+        firstName: firstName
+    })  } else {
+      console.log("you are not authenticated");
+    }
+  });
+}
+*/
 
   handleSignUp = () => {
-    const { email, password } = this.state
+    const { email, password, firstName, lastName, studySubject } = this.state
     firebase
       .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(user => this.props.navigation.navigate('Main'))
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => {
+          user = firebase.auth().currentUser,
+          firebase.database().ref('users/' + user.uid).set({
+            email: email.toLowerCase(),
+            fornavn: firstName,
+            etternavn: lastName,
+            studieretning: studySubject
+        })
+          //console.log(user),
+          console.log(user.uid);
+        })
+        /*.then((user) => {
+              console.log(user.uid);
+              firebase.database().ref('users/' + user.uid).set({
+                email: email.toLowerCase(),
+                firstName: firstName
+          })
+          console.log(user.uid)
+          console.log("22")
+        })
+      })
+      *///.then(user => this.props.navigation.navisgate('Main'))
       .catch(error => this.setState({ errorMessage: error.message }))
   }
+
+/*
+saveUserData = (user) => {
+    console.log(user)
+    let userid = user.uid
+    console.log(userid)
+    firebase.database().ref('users/' + userid).set({
+    email: email.toLowerCase(),
+    firstName: firstName
+    })
+  }))
+}
+*/
 
   render() {
     return (
@@ -22,6 +78,28 @@ export default class SignUp extends React.Component {
           <Text style={{ color: 'red' }}>
             {this.state.errorMessage}
           </Text>}
+
+        <TextInput
+          placeholder="Fornavn"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={firstName => this.setState({ firstName })}
+          value={this.state.firstName}
+        />
+        <TextInput
+          placeholder="Etternavn"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={lastName => this.setState({ lastName })}
+          value={this.state.lastName}
+        />
+        <TextInput
+          placeholder="Studieretning"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={studySubject => this.setState({ studySubject })}
+          value={this.state.studySubject}
+        />
         <TextInput
           placeholder="Email"
           autoCapitalize="none"
