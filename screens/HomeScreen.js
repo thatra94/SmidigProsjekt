4ea289a21536ee7cd30a5d5852e33f192f719138
Fredrik <31255680+fredriklugg@users.noreply.github.com>
2019-05-24 14:24:23 +0200

    Revert "Merge branch 'master' of https://github.com/thatra94/SmidigProsjekt"
    This reverts commit b2351587e0552d60385e4984944bdf7b1acecdee, reversing
    changes made to 5673d04fc5d41e0d5cc6b83122715aecf6422419.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { LinearGradient } from 'expo';
 import {
   Image,
@@ -31,63 +30,132 @@ export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView scrollEventThrottle={16}>
-          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <View style={{flex: 1, flexDirection: 'row'}}>
+            // Bakgrunn 
+            // Lilla header 
+            // Gradient sirkel
+            // Logo
+            // Hvit boks
+            // Studieretning-knapp
+            // Velg et emne-knapp
+            <View style={styles.container}>
+              <ScrollView scrollEventThrottle={16}>
+              <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+              <View style={{width: 450, 
+                            height: 470, 
+                            backgroundColor: '#330F2A'}} />
 
-          <View style={styles.purpleBackground} />
+              <LinearGradient
+                colors={['#D54FBA', '#3F0630']}
+                style={{position: 'absolute',
+                        flex: 1,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        right: -30,
+                        top: -120,
+                        width: 500,
+                        height: 500,
+                        borderRadius: 500/2}}>
+              </LinearGradient>
 
-          <LinearGradient
-            colors={['#D54FBA', '#3F0630']}
-            style={styles.gradientCircle}>
-          </LinearGradient>
+              <Image source={require('../assets/images/Hvit.png')}
+                            style={{position: 'absolute',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            right: 150,
+                            top: 90,
+                            flex: 1,
+                            width: 155,
+                            height: 155}}
+                            resizeMode="stretch"
+                />
 
-          <Image source={require('../assets/images/Hvit.png')}
-                        style={styles.logo}
-                        resizeMode="stretch"/>
+                <View style={{
+                          position: 'absolute',
+                          top: 270,
+                          right: 65,
+                          width: 320, 
+                          height: 400, 
+                          backgroundColor: 'white', 
+                          borderRadius: 50,
+                          borderWidth: 0.5,
+                          borderColor: '#d6d7da',
+                          shadowColor: 'black',
+                          shadowOffset:{width: 6, height: 5},
+                          shadowOpacity: 0.4}}>
 
-            <View style={styles.whiteFrame}>
+                <Text style={{textAlign: 'center', 
+                          marginTop: 60, 
+                          color: '#5D1049',
+                          fontSize: 20,
+                          fontWeight: 'bold' }}>
+                            Hvordan vil du finne gruppe?</Text> 
 
-            <View style={styles.whiteContainer}>
+                <View style={{flex: 1, 
+                          flexDirection: 'column', 
+                          alignItems: 'center', 
+                          marginTop: 40, 
+                          shadowOffset:{width: 6, height: 5}, 
+                          shadowColor: 'black', 
+                          shadowOpacity: 0.4}}>
 
-            <Text style={styles.txtGroup}>
-                        Hvordan vil du finne gruppe?</Text> 
-              
-            <View style={styles.btnContainer}>
-              <View style={styles.btnShadow}>
+                <TouchableOpacity onPress={() => 
+                            {this.props.navigation.navigate('Gruppe')}}
+                            style={{height: 120, top: -20}}>
+                <View style={{flex: 1, 
+                            flexDirection: 'row', 
+                            marginTop: 30, 
+                            width: 250, 
+                            height: 100, 
+                            backgroundColor: '#5D1049', 
+                            borderRadius: 30 }}>
+                <Image source={require('../assets/images/open-book.png')}
+                    style={{width: 35, 
+                            height: 35, 
+                            marginTop: 27, 
+                            marginLeft: 20}}
+                            />
+                <Text style={{textAlign: 'center', 
+                            marginTop: 35, 
+                            marginLeft: 30,
+                            fontWeight: 'bold',
+                            fontSize: 18, 
+                            color: 'white' }}>Studieretning</Text>
+                        </View>
+                            </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => 
-                        {this.props.navigation.navigate('Gruppe')}}
-                        style={{height: 120, top: -20}}>
-              <View style={styles.btnStudieretning}>
-            <Image source={require('../assets/images/open-book.png')}
-                style={styles.bookIcon}
-                        />
-            <Text style={styles.txtStudieretning}>Studieretning</Text>
-                    </View>
-                        </TouchableOpacity>
-
-              <TouchableOpacity onPress={() => 
-                        {this.props.navigation.navigate('Fag')}}
-                  style={{height: 110, top: 10}}>
-                <View style={styles.btnVelgEmne}>
-              <Image source={require('../assets/images/college.png')}
-                style={styles.collegeIcon}
-                          />
-              <Text style={styles.txtVelgEmne}>
-                          Velg et emne
-                      </Text>
-                      </View>
-                </TouchableOpacity>
-                </View>
-                </View>
+                  <TouchableOpacity onPress={() => 
+                            {this.props.navigation.navigate('Fag')}}
+                      style={{height: 110, top: 10}}>
+                  <View style={{flex: 1,
+                            flexDirection: 'row', 
+                            marginTop: 15, 
+                            width: 250, 
+                            height: 100, 
+                            backgroundColor: '#5D1049', 
+                            borderRadius: 30 }}>
+                  <Image source={require('../assets/images/college.png')}
+                    style={{width: 35, 
+                            height: 35, 
+                            marginTop: 27, 
+                            marginLeft: 20}}
+                              />
+                  <Text style={{textAlign: 'center', 
+                            marginTop: 35, 
+                            color: 'white', 
+                            marginLeft: 30,
+                            fontWeight: 'bold',
+                            fontSize: 18 }}>
+                              Velg et emne
+                          </Text>
+                            </View>
+                      </TouchableOpacity>
+                  </View>
+              </View>
             </View>
-        </View>
-      </View>
-    </View>
-  </ScrollView>
-      </View>
+          </View>
+        </ScrollView>
+            </View>
     );
   }
 }
@@ -99,84 +167,6 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%'
   },
-  whiteFrame:{
-    position: 'absolute',
-    top: '45%',
-    margin: '5%',
-    width: wp('90%'), 
-    height: hp('55%'), 
-    backgroundColor: 'white', 
-    borderRadius: 50,
-    borderWidth: 0.5,
-    borderColor: '#d6d7da',
-    shadowColor: 'black',
-    shadowOffset:{width: 6, height: 5},
-    shadowOpacity: 0.4
-  },
-  txtGroup:{
-    textAlign: 'center', 
-    marginTop: 60, 
-    color: '#5D1049',
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-  btnShadow:{
-    flex: 1, 
-    flexDirection: 'column', 
-    alignItems: 'center', 
-    marginTop: 40, 
-    shadowOffset:{width: 6, height: 5}, 
-    shadowColor: 'black', 
-    shadowOpacity: 0.4
-  },
-  btnStudieretning:{
-    flex: 1, 
-    flexDirection: 'row', 
-    marginTop: 30, 
-    width: wp('70%'), 
-    height: hp('20%'), 
-    backgroundColor: '#5D1049', 
-    borderRadius: 30
-  },
-  txtStudieretning:{
-    textAlign: 'center', 
-    marginTop: 35, 
-    marginLeft: 30,
-    fontWeight: 'bold',
-    fontSize: 18, 
-    color: 'white'
-  },
-  bookIcon:{
-    width: 35, 
-    height: 35, 
-    marginTop: 27, 
-    marginLeft: 20
-  },
-  btnVelgEmne:{
-    flex: 1,
-    flexDirection: 'row', 
-    marginTop: 15, 
-    width: wp('70%'), 
-    height: hp('20%'), 
-    backgroundColor: '#5D1049', 
-    borderRadius: 30
-  },
-  collegeIcon:{
-    width: 35, 
-    height: 35, 
-    marginTop: 27, 
-    marginLeft: 20
-  },
-  txtVelgEmne:{
-    textAlign: 'center', 
-    marginTop: 35, 
-    color: 'white', 
-    marginLeft: 30,
-    fontWeight: 'bold',
-    fontSize: 18
-  },
-
-//Har ikke kodet resten. Vet ikke hvis man skal beholde den
   overlayContainer: {
         flex: 1,
         backgroundColor: 'rgba(47,163,218, .4)'
