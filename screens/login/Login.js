db295942232bed6b4bc6ce98a11ea28f09f1b653
Jonas Linnestad <jonas.linnestad@gmail.com>
2019-05-28 13:55:21 +0200

    Revert "Revert "adijoaios""
    This reverts commit 732d0742f4d499d1650d75c226a6b26afcb088e1.

--- screens/login/Login.js
+++ screens/login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScrollView, StyleSheet, Text, TextInput, View, Button, TouchableOpacity,  StatusBar } from 'react-native'
+import { ScrollView, StyleSheet, Text, TextInput, View, Button, TouchableOpacity,  StatusBar, Image } from 'react-native'
 import { LinearGradient } from 'expo';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import firebase from 'firebase'
@@ -24,53 +24,56 @@ export default class Login extends React.Component {
     return (
         <View style={styles.backgroundContainer}>
             <ScrollView scrollEventThrottle={16}>
-                    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                    </View>
-            <LinearGradient
-                colors={['#D54FBA', '#3F0630']}
-                style={{position: 'absolute',
-                        top: -120,
-                        left: -40,
-                        width: 500,
-                        height: 500,
-                        borderRadius: 500/2}}>
-              </LinearGradient>
+        
+                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                </View>
+        
+                <LinearGradient
+                    colors={['#D54FBA', '#3F0630']}
+                    style={styles.gradientCircle}>
+                </LinearGradient>
 
-            <View style={styles.container}>
-                
-            <Text style={{color: 'white', marginTop: '30%', fontSize: 30, marginBottom: '15%'}}>Logg inn</Text>
-            {this.state.errorMessage &&
-              <Text style={{ color: 'red' }}>
-                {this.state.errorMessage}
-              </Text>}
-            <TextInput
-              style={styles.textInput}
-              autoCapitalize="none"
-              placeholder="Brukernavn"
-              placeholderTextColor='white'
-              onChangeText={email => this.setState({ email })}
-              value={this.state.email}
-            />
-            <TextInput
-              secureTextEntry
-              style={styles.textInput}
-              autoCapitalize="none"
-              placeholder="Passord"
-              placeholderTextColor='white'
-              onChangeText={password => this.setState({ password })}
-              value={this.state.password}
-            />
+                <View style={styles.container}>
+                    <Image
+                    style={styles.logo}
+                    source={{uri: 'https://i.imgur.com/KPVfX73.png'}}
+                    />
+        
+                <Text style={{color: 'white', marginTop: '35%', fontSize: 30, marginBottom: '15%'}}>Logg inn</Text>
+                {this.state.errorMessage &&
+                <Text style={{ color: 'red' }}>
+                    {this.state.errorMessage}
+                </Text>}
+        
+                <TextInput
+                  style={styles.textInput}
+                  autoCapitalize="none"
+                  placeholder="Brukernavn"
+                  placeholderTextColor='white'
+                  onChangeText={email => this.setState({ email })}
+                  value={this.state.email}
+                />
+                      
+                <TextInput
+                  secureTextEntry
+                  style={styles.textInput}
+                  autoCapitalize="none"
+                  placeholder="Passord"
+                  placeholderTextColor='white'
+                  onChangeText={password => this.setState({ password })}
+                  value={this.state.password}
+                />
 
-            <TouchableOpacity onPress={() => this.props.navigation.navigate('SignUp')} style={styles.registerButton}>
-                <Text style={styles.registerText}>Registrer deg</Text>
-            </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.props.navigation.navigate('SignUp')} style={styles.registerButton}>
+                    <Text style={styles.registerText}>Registrer deg</Text>
+                </TouchableOpacity>
 
-            <TouchableOpacity onPress={this.handleLogin} style={styles.loginButton}>
-                <Text style={styles.loginText}>Logg inn</Text>
-            </TouchableOpacity>
-          </View>
+                <TouchableOpacity onPress={this.handleLogin} style={styles.loginButton}>
+                    <Text style={styles.loginText}>Logg inn</Text>
+                </TouchableOpacity>
+            </View>
         </ScrollView>
-        </View>
+    </View>
     )
   }
 }
@@ -105,7 +108,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderRadius: 100,
     alignItems: 'center',
-    padding: 30,
+    padding: 20,
     paddingLeft: 50,
     paddingRight: 50,
   },
@@ -113,13 +116,32 @@ const styles = StyleSheet.create({
     color: '#3e1133',
     fontSize: 20,
   },
-  registerButton: {
-
-  },
   registerText: {
     color: 'white',
     fontSize: 20,
     textDecorationLine: 'underline',
+  },
+  logo:{
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 110,
+    top: 50,
+    flex: 1,
+    width: 55,
+    height: 60
+  },
+  /*Gradient bakgrunn*/
+  gradientCircle:{
+    position: 'absolute',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    right: -30,
+    top: -120,
+    width: 500,
+    height: 500,
+    borderRadius: 500/2
   },
 
 })
