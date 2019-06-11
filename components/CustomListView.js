27fb37d117e5ae61eac7e31f0220a2b786fc39fe
Erlend Skansen <skaerl17@EGMS.no>
2019-06-11 12:45:09 +0200

    Update CustomListView.js

--- components/CustomListView.js
+++ components/CustomListView.js
@@ -32,7 +32,7 @@ const CustomRow = ({ title, navigation }) => (
             "Er du sikker?",
             "Dette legger deg til i en gruppe for dette emnet",
             [
-                {text: "Avbryt", type: "cancel", style: 'destructive'},
+                {text: "Avbryt", type: "cancel"},
                 {text: "Fortsett", onPress: async () => {
                     await FireBase.getInstance().joinGroup(firebase.auth().currentUser.uid, title);
                     navigation.navigate("Gruppe");}}
